9fc40a1ee265d289bb022fa203bd2409f7dfb018
yoshi991 <yoshiki.henmi@gmail.com>
2023-03-30 03:52:27 +0900

    Update pubspeck

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sample/app.dart';
 import 'package:flutter_sample/data/data_source/local/shared_preferences.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import 'app.dart';
+// ignore:depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Intl.defaultLocale = 'ja';
-  final sharedPreferences = await SharedPreferences.getInstance();
 
+  // turn off the # in the URLs on the web
+  usePathUrlStrategy();
+  // * Register error handlers. For more info, see:
+  // * https://docs.flutter.dev/testing/errors
+  registerErrorHandlers();
+
+  final sharedPreferences = await SharedPreferences.getInstance();
   runApp(
     ProviderScope(
       overrides: [
@@ -19,4 +27,27 @@ Future<void> main() async {
       child: const App(),
     ),
   );
+}
+
+void registerErrorHandlers() {
+  // * Show some error UI if any uncaught exception happens
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    debugPrint(details.toString());
+  };
+  // * Handle errors from the underlying platform/OS
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+    debugPrint(error.toString());
+    return true;
+  };
+  // * Show some error UI when any widget in the app fails to build
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text('An error occurred'),
+      ),
+      body: Center(child: Text(details.toString())),
+    );
+  };
 }
